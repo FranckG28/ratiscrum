940292ba413aba6dfd33ce2637d9c0ec88d37a28
Eren Mustafa Ceylan <eren-mustafa.ceylan@insa-lyon.fr>
2023-02-18 10:47:54 +0100

    Price ranking added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,43 @@
 import Button from "@/components/button";
+import Price from "@/components/price";
 
 export default function Home() {
   return (
-    <div className='flex flex-col gap-2 py-5'>
+    <div className="flex flex-col gap-2 py-5">
       <h1 className="text-white">vidéo ;)</h1>
 
       <div className="flex flex-col">
         <p className="text-slate-300">14 décembre 2022</p>
         <h1 className="text-4xl text-white font-medium">Nuit de l'info 2022</h1>
         <div className="flex items-center gap-2 py-2">
-          <Button style="primary" href="/">Visiter</Button>
-          <Button style="flat" href="/">Site officiel</Button>
+          <Button style="primary" href="/">
+            Visiter
+          </Button>
+          <Button style="flat" href="/">
+            Site officiel
+          </Button>
+        </div>
+        <div className="flex mt-4 rounded-lg flex-col border-2 border-slate-600 gap-2 py-4">
+          <p className="text-slate-300 m-4 my-0">Résultats</p>
+          <div className="flex flex-wrap gap-2 gap-y-4">
+            <Price
+              rank={1}
+              title="VEGA CS GROUP 2022"
+              description="1500€"
+            ></Price>
+            <Price
+              rank={2}
+              title="VEGA CS GROUP 2022"
+              description="1500€"
+            ></Price>
+            <Price
+              rank={3}
+              title="VEGA CS GROUP 2022"
+              description="1500€"
+            ></Price>
+          </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 }
---
+++ src/components/price.tsx
@@ -0,0 +1,21 @@
+export const emoji: string[] = ["🥇", "🥈", "🥉"];
+
+export default function Price({ rank, title, description }: PriceProps) {
+  return (
+    <div className="flex gap-2 items-center px-3 w-fit">
+      <div className="flex items-center justify-center w-10 h-10 rounded-full bg-slate-700/50">
+        <p className="text-white text-xl font-medium">{emoji[rank - 1]}</p>
+      </div>
+      <div className="flex flex-col">
+        <p className="text-white text-lg font-medium">{title}</p>
+        {description && <p className="text-slate-300 text-sm">{description}</p>}
+      </div>
+    </div>
+  );
+}
+
+export interface PriceProps {
+  rank: 1 | 2 | 3;
+  title: string;
+  description?: string;
+}
