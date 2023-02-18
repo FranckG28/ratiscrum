a4eec03ff221eb4bc567d168a8657cc26235ae36
Eren Mustafa Ceylan <eren-mustafa.ceylan@insa-lyon.fr>
2023-02-18 13:10:48 +0100

    Mute and autoplay video

--- src/components/youtube.tsx
+++ src/components/youtube.tsx
@@ -1,11 +1,13 @@
 export default function YouTube({ videoId }: { videoId: string }) {
-    return <div className="w-full aspect-video overflow-hidden rounded-lg shadow-xl">
-        <iframe
-            src={`https://www.youtube.com/embed/${videoId}`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
-            style={{ height: "100%", width: "100%" }}
-        />
+  return (
+    <div className="w-full aspect-video overflow-hidden rounded-lg shadow-xl">
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?&autoplay=1&mute=1`}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+        style={{ height: "100%", width: "100%" }}
+      />
     </div>
+  );
 }
