bfa85093c02b92b188724851a6691a8a41b742e4
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-02-18 09:42:30 +0100

    add initial layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from '@/components/sidebar'
 import './globals.css'
 
 export default function RootLayout({
@@ -6,13 +7,23 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className='h-full'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
-    </html>
+      <body className="dark bg-slate-900 flex h-full overflow-hidden">
+
+        <Sidebar />
+
+        <main className='flex-1 overflow-auto'>
+          <div className="mx-auto max-w-6xl">
+            {children}
+          </div>
+        </main>
+
+      </body >
+    </html >
   )
 }
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+    return (
+        <nav className='flex flex-col bg-slate-800 border-r border-slate-700 shadow-lg p-5 w-72'>
+            <h1 className='text-white'>Ratiscrum</h1>
+        </nav>
+    )
+}
