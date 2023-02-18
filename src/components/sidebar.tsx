065be3f06b8dbcd8e5e8a242be28e0705629e8fa
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-02-18 09:45:05 +0100

    add logo in navbar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+import RatiscrumLogo from "/public/rts-logo.png"
+
 export default function Sidebar() {
     return (
         <nav className='flex flex-col bg-slate-800 border-r border-slate-700 shadow-lg p-5 w-72'>
-            <h1 className='text-white'>Ratiscrum</h1>
+            <Image src={RatiscrumLogo} alt="Ratiscrum Logo" />
         </nav>
     )
 }
