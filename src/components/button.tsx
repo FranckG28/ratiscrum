e23025d2360c81ea9f7f15b2747bcda6c2c3e652
Eren Mustafa Ceylan <eren-mustafa.ceylan@insa-lyon.fr>
2023-02-18 14:04:59 +0100

    Active style button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,11 +3,16 @@ import Link from "next/link";
 
 const buttonStyle: { [key: string]: string } = {
   primary:
-    "py-2 px-5 bg-indigo-500 hover:bg-indigo-600 text-white border-t border-indigo-400/70 shadow-lg hover:shadow-xl",
-  flat: "bg-transparent text-indigo-400 hover:text-indigo-500",
+    "py-2 px-5 bg-indigo-500 hover:bg-indigo-600 text-white border-t border-indigo-400/70 shadow-lg hover:shadow-xl active:bg-indigo-700",
+  flat: "bg-transparent text-indigo-400 hover:text-indigo-500 active:text-indigo-600",
 };
 
-export default function Button({ children, style, href, className }: ButtonProps) {
+export default function Button({
+  children,
+  style,
+  href,
+  className,
+}: ButtonProps) {
   return (
     <Link
       href={href}
