275683627f48281ca98a09c2b2fa1198e524086d
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-02-27 20:52:20 +0100

    sidebar nav items from real data

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,11 +2,13 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Badge from "./badge";
 import RatiscrumLogo from "/public/rts-logo.png";
 import { NavItem } from "@/models/nav-item";
 import { useContext } from "react";
 import { ThemeContext } from "@/components/theme-provider";
+import { contentList } from "@/content/content-list";
+import { capitalize, displayDate } from "@/services/utils";
+import NavItemComponent from "@/components/nav-item";
 
 export default function Sidebar() {
   const theme = useContext(ThemeContext);
@@ -15,8 +17,15 @@ export default function Sidebar() {
       <Link className="w-48" href={"/"}>
         <Image src={RatiscrumLogo} alt="Ratiscrum Logo" />
       </Link>
-      {items.map((item, index) => (
-        <NavItem key={index} {...item} />
+      {contentList.map((item, index) => (
+        <NavItemComponent
+          key={index}
+          title={item.name}
+          subtitle={capitalize(displayDate(item.date))}
+          info={`${item.rewards.length} récompenses`}
+          url={`/${item.slug}`}
+          isActive={false}
+        />
       ))}
       {/* <button
         onClick={() => {
@@ -46,30 +55,4 @@ const items: NavItem[] = [
   },
 ];
 
-function NavItem(props: NavItem) {
-  return (
-    <Link
-      href={props.url}
-      className={`relative flex flex-col px-5 py-4 rounded-lg ${
-        props.isActive
-          ? "dark:bg-slate-700 bg-slate-300"
-          : "hover:bg-slate-500/5 dark:hover:bg-slate-400/5"
-      }`}
-    >
-      <p className="text-slate-700 dark:text-slate-400 text-sm leading-3">
-        {props.subtitle}
-      </p>
-      <p className="text-black dark:text-white text-lg font-medium">
-        {props.title}
-      </p>
-      <Badge text={props.info}></Badge>
-      <span
-        className={`absolute inset-y-4 -left-px w-1 rounded-full bg-gradient-to-b ${
-          props.isActive
-            ? "bg-indigo-500"
-            : "bg-slate-600/20 dark:bg-slate-400/20"
-        }`}
-      ></span>
-    </Link>
-  );
-}
+
