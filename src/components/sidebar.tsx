87422219f43689ab409bb94a9be34dd913593687
Franck GUTMANN <franck.gutmann@pixacare.com>
2023-02-27 21:00:16 +0100

    add selected article active state in navbar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,15 +3,17 @@
 import Image from "next/image";
 import Link from "next/link";
 import RatiscrumLogo from "/public/rts-logo.png";
-import { NavItem } from "@/models/nav-item";
 import { useContext } from "react";
 import { ThemeContext } from "@/components/theme-provider";
 import { contentList } from "@/content/content-list";
 import { capitalize, displayDate } from "@/services/utils";
 import NavItemComponent from "@/components/nav-item";
+import { useSelectedLayoutSegment } from 'next/navigation';
 
 export default function Sidebar() {
   const theme = useContext(ThemeContext);
+  const segment = useSelectedLayoutSegment();
+
   return (
     <nav className="flex flex-col bg-slate-200 dark:bg-slate-800 md:border-r gap-3 dark:border-slate-700 border-slate-300 shadow-lg md:p-5 md:w-72 w-0 invisible md:visible">
       <Link className="w-48" href={"/"}>
@@ -24,7 +26,7 @@ export default function Sidebar() {
           subtitle={capitalize(displayDate(item.date))}
           info={`${item.rewards.length} récompenses`}
           url={`/${item.slug}`}
-          isActive={false}
+          isActive={segment === item.slug}
         />
       ))}
       {/* <button
