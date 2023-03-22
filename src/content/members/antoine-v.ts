86e1f6183a194fd00454988cf221b270055b8396
Franck G <gutmann.franck@outlook.fr>
2023-03-22 22:53:21 +0100

    feat: add medias

---
+++ src/content/members/antoine-v.ts
@@ -0,0 +1,15 @@
+import antoineVAvatar from "@/assets/avatar/antoineV.jpg";
+import { Member } from "@/models/member";
+
+export const antoineV: Member = {
+    name: "Antoine Vérin",
+    role: "Développeur Fullstack",
+    avatar: antoineVAvatar,
+    links: [
+        {
+            name: "Linkedin",
+            icon: "linkedin",
+            url: "https://www.linkedin.com/in/antoine-verin/",
+        },
+    ],
+}
