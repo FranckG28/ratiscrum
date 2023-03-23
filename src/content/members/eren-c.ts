1e6631cde62417514b11f3cf4ecbf5c5bba4a079
Eren Mustafa Ceylan <eren-mustafa.ceylan@insa-lyon.fr>
2023-03-23 21:44:52 +0100

    Git link added

--- src/content/members/eren-c.ts
+++ src/content/members/eren-c.ts
@@ -2,14 +2,19 @@ import erenCAvatar from "@/assets/avatar/erenC.jpg";
 import { Member } from "@/models/member";
 
 export const erenC: Member = {
-    name: "Eren Ceylan",
-    role: "Développeur Fullstack",
-    avatar: erenCAvatar,
-    links: [
-        {
-            name: "Linkedin",
-            icon: "linkedin",
-            url: "https://www.linkedin.com/in/erenm-ceylan/",
-        },
-    ],
-}
+  name: "Eren Ceylan",
+  role: "Développeur Fullstack",
+  avatar: erenCAvatar,
+  links: [
+    {
+      name: "GitHub",
+      icon: "github",
+      url: "https://github.com/ErenMustafaGit",
+    },
+    {
+      name: "Linkedin",
+      icon: "linkedin",
+      url: "https://www.linkedin.com/in/erenm-ceylan/",
+    },
+  ],
+};
