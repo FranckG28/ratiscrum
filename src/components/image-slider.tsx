a4843f1e1f1621aa8323573e4bcfd7a2c5fc0f86
Franck G <gutmann.franck@outlook.fr>
2023-04-01 15:51:21 +0200

    refactor(image-slider): improve button design

--- src/components/image-slider.tsx
+++ src/components/image-slider.tsx
@@ -108,21 +108,37 @@ function Arrow(props: {
   left?: boolean;
   onClick: (e: any) => void;
 }) {
-  const disabeld = props.disabled ? " arrow--disabled" : "";
   return (
-    <svg
-      onClick={props.onClick}
-      className={`bg-gray-200 dark:bg-gray-700 transition border-t hover:scale-105 border-gray-100 dark:border-gray-600 hover:shadow-xl shadow-lg rounded-full p-3.5 w-10 h-10 absolute top-1/2 fill-slate-800 dark:fill-slate-200 cursor-pointer hover:bg-gray-300 dark:hover:bg-gray-800 ${props.left ? "-left-6" : "-right-6 left-auto"
-        } ${disabeld}`}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+    <button onClick={props.onClick} type="button" disabled={props.disabled}
+      className={`
+        ${props.disabled
+          ? `cursor-default
+            bg-gray-200 dark:bg-gray-600
+            fill-gray-500 dark:fill-gray-400
+          `
+          : `cursor-pointer 
+            bg-indigo-200 dark:bg-gray-700 
+            hover:bg-indigo-300 dark:hover:bg-gray-800
+            fill-indigo-800 dark:fill-indigo-200 
+            hover:shadow-xl shadow-lg
+            hover:scale-105 
+          `
+        }
+          border-gray-100 dark:border-gray-600 
+          transition border-t 
+          rounded-full p-3.5 
+          w-10 h-10 absolute top-1/2 
+          ${props.left ? "-left-6" : "-right-6 left-auto"}
+        `}
     >
-      {props.left && (
-        <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
-      )}
-      {!props.left && (
-        <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
-      )}
-    </svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        {props.left && (
+          <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+        )}
+        {!props.left && (
+          <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+        )}
+      </svg>
+    </button>
   );
 }
