59272f2728e04d88408c10d0f8ea7443bb115940
Franck G <gutmann.franck@outlook.fr>
2023-04-01 15:51:05 +0200

    fix: fix 2021 event date

--- src/content/events/2021.ts
+++ src/content/events/2021.ts
@@ -12,7 +12,7 @@ import { antoineV } from "../members/antoine-v";
 export const event2021: Event = {
     slug: "ndli-2021",
     name: "Nuit de l'info 2021",
-    date: new Date(2021, 12, 4),
+    date: new Date(2021, 11, 4),
     location: "IUT Robert Schuman, Illkirch Graffenstaden",
     description: `
     Cette année, le sujet a été élaboré par la station de Dunkerque de la Société Nationale de Sauvegarde en Mer (SNSM) et s’intitulait « L’informatique à l’aide de l’histoire des sauvetages en mer ». L'objectif était de créer un site communautaire repertoriant toutes les informations sur les sauvetages en mer. Les membres de la communauté pourront ajouter des informations sur les sauvetages, les bateaux, les sauvés ... 
