01af366ac2c71986bdb590d2f92440b210ac15d2
Franck G <gutmann.franck@outlook.fr>
2023-04-01 15:50:49 +0200

    refactor: don't display weekday in nav item

--- src/components/nav-item.tsx
+++ src/components/nav-item.tsx
@@ -6,11 +6,10 @@ export default function NavItemComponent(props: NavItem) {
   return (
     <Link
       href={props.url}
-      className={`relative flex flex-col px-5 py-4 rounded-lg text-start active:scale-95 transition-all ${
-        props.isActive
-          ? "dark:bg-slate-700 bg-slate-300"
-          : "hover:bg-slate-500/5 dark:hover:bg-slate-400/5"
-      }`}
+      className={`relative flex flex-col px-5 py-4 gap-1 rounded-lg text-start active:scale-95 transition-all ${props.isActive
+        ? "dark:bg-slate-700 bg-slate-100 shadow-xl shadow-white/20 dark:shadow-slate-500/10"
+        : "hover:bg-slate-500/5 dark:hover:bg-slate-400/5"
+        }`}
     >
       <p className="text-slate-700 dark:text-slate-400 text-sm leading-3">
         {props.subtitle}
@@ -20,11 +19,10 @@ export default function NavItemComponent(props: NavItem) {
       </p>
       <Badge text={props.info}></Badge>
       <span
-        className={`absolute inset-y-4 -left-px w-1 rounded-full bg-gradient-to-b ${
-          props.isActive
-            ? "bg-indigo-500"
-            : "bg-slate-600/20 dark:bg-slate-400/20"
-        }`}
+        className={`absolute inset-y-4 -left-px w-1 rounded-full bg-gradient-to-b ${props.isActive
+          ? "bg-indigo-500"
+          : "bg-slate-600/20 dark:bg-slate-400/20"
+          }`}
       ></span>
     </Link>
   );
