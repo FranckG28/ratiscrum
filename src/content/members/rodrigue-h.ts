a539250ad473994729739f0e9ccaa320a6f8e5e3
Franck <gutmann.franck@outlook.fr>
2023-12-09 23:04:55 +0100

    fix: remove old member files

--- src/content/members/rodrigue-h.ts
+++
@@ -1,9 +0,0 @@
-import ratAvatar from "@/assets/avatar/rat.png";
-import { Member } from "@/models/member";
-
-export const rodrigueH: Member = {
-    name: "Rodrigue Horny",
-    role: "Narative designer et vidéo",
-    avatar: ratAvatar,
-    links: [],
-}
