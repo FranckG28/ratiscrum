f7b94169f3f921a8d3103de1586bb3696f18cc20
Franck <gutmann.franck@outlook.fr>
2023-12-11 13:32:24 +0100

    refactor: remove next-themes

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -3,5 +3,9 @@
 import { MDXRemote } from "next-mdx-remote";
 
 export default function Content({ mdx }: { mdx: any }): JSX.Element {
-  return <MDXRemote {...mdx} />;
+  return (
+    <div className="text-slate-300">
+      <MDXRemote {...mdx} />;
+    </div>
+  );
 }
