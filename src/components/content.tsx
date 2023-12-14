e7e5b13dc088dfe62c3594c41a68123372a955cb
Franck <gutmann.franck@outlook.fr>
2023-12-14 16:06:17 +0100

    fix: project description

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -5,7 +5,7 @@ import { MDXRemote } from "next-mdx-remote";
 export default function Content({ mdx }: { mdx: any }): JSX.Element {
   return (
     <div className="text-slate-300">
-      <MDXRemote {...mdx} />;
+      <MDXRemote {...mdx} />
     </div>
   );
 }
