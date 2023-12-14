68e4b9541877439c0b5e06d1e7d3cc57b0b3c64e
Franck <gutmann.franck@outlook.fr>
2023-12-14 16:12:01 +0100

    feat: status

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -4,7 +4,7 @@ import { MDXRemote } from "next-mdx-remote";
 
 export default function Content({ mdx }: { mdx: any }): JSX.Element {
   return (
-    <div className="text-slate-300">
+    <div className="text-slate-300 flex flex-col gap-2">
       <MDXRemote {...mdx} />
     </div>
   );
