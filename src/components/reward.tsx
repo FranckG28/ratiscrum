28b9a80edf1a1111b794d28d7b77199d2cb7c5f2
Franck <gutmann.franck@outlook.fr>
2024-01-29 19:52:08 +0100

    style(reward): improve style & display rank

--- src/components/reward.tsx
+++ src/components/reward.tsx
@@ -1,16 +1,24 @@
 import { Reward } from "@/models/reward";
 
 export const emoji: string[] = ["🥇", "🥈", "🥉"];
+export const titles: string[] = [
+  "Première place",
+  "Deuxième place",
+  "Troisième place",
+];
 
 export default function RewardComponent({ rank, title, description }: Reward) {
   return (
-    <div className="flex gap-2 items-center p-3 rounded-xl">
-      <div className="flex items-center justify-center w-10 h-10 rounded-full bg-slate-300/50 border border-slate-400/20 dark:bg-slate-700/50 aspect-square">
-        <p className="text-slate-800 dark:text-white text-xl font-medium">
+    <div className="flex gap-3 items-start p-3 rounded-xl">
+      <div className="flex items-center justify-center w-12 h-12 rounded-full bg-slate-300/50 border border-slate-400/20 dark:bg-slate-700/50 aspect-square">
+        <p className="text-slate-800 dark:text-white text-3xl font-medium">
           {emoji[rank - 1]}
         </p>
       </div>
       <div className="flex flex-col">
+        <p className="text-slate-700 dark:text-slate-400 text-sm leading-tight">
+          {titles[rank - 1]}
+        </p>
         <p className="text-slate-800 dark:text-white text-lg font-medium">
           {title}
         </p>
