241f3e4da01f837b25b605ea6bac1e126ed6bdf9
Raffael Di Pietro <maxou4170@gmail.com>
2024-12-08 00:35:59 +0100

    convert 2024 images into webp for better compression

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,6 +5,6 @@ export const membersPath = contentPath + "members/";
 
 export const assetsPath = "/public/";
 
-export const imageExtensions = ['.jpg', '.jpeg', '.png', '.JPG', '.JPEG', '.PNG'];
+export const imageExtensions = ['.jpg', '.jpeg', '.png', '.JPG', '.JPEG', '.PNG', '.webp', '.WEBP'];
 
 export const defaultOg = "/og.png"
